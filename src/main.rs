77d52595ec03420bd1d561b6ed6eb1801c485856
Caleb Aitcheson <ecaleb1@protonmail.com>
2022-12-31 11:30:36 -0800

    Added ability to read from archive

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,21 @@
 #![allow(non_snake_case)]
-use iced::widget::{column, container, image, row, button};
-use iced::{Element, Sandbox, Settings, Length};
+use iced::widget::{image, row, button};
+use iced::{Element, Sandbox, Settings, Length, Alignment};
 
-use std::{fs, io};
+use std::{io, fs};
+use std::fs::File;
 use std::path::PathBuf;
-
+use zip::{read};
 
 pub fn main() -> iced::Result {
+    read::ZipArchive::new( File::open("Comic.cbr").unwrap() ).expect("Failed to read archive").extract(".files").unwrap();
     Reader::run(Settings {id: Some(String::from("MangaReader")), window: iced::window::Settings::default(), flags: (), default_font: None, default_text_size: 16, text_multithreading: true, exit_on_close_request: true, antialiasing: true, try_opengles_first: false})
 }
 
 struct Reader {
-    i: usize,
+    page: usize,
     entries: Vec<PathBuf>,
+    length: usize,
 }
 
 #[derive(Debug, Clone)]
@@ -25,9 +28,13 @@ impl Sandbox for Reader {
     type Message = Message;
 
     fn new() -> Reader {
+        let mut var = fs::read_dir(".files").expect("").map(|res| res.map(|e| e.path())).collect::<Result<Vec<_>, io::Error>>().unwrap();
+        let zip_len = var.len();
+        var.sort_unstable();
         Reader {
-            entries: fs::read_dir("./img").expect("").map(|res| res.map(|e| e.path())).collect::<Result<Vec<_>, io::Error>>().unwrap(),
-            i: 0,
+            page: 0,
+            entries: var,
+            length: zip_len,
         }
     }
 
@@ -38,22 +45,29 @@ impl Sandbox for Reader {
     fn update(&mut self, message: Self::Message) {
         match message {
             Message::NextImage => {
-                self.i += 1;
+                if self.page+1 == self.length {
+                    //Indicate last page
+                } else {
+                    self.page += 1;
+                }
             }
             Message::PreviousImage => {
-                self.i -= 1;
+                if self.page == 0 {
+                    //Indicate first page
+                } else {
+                    self.page -= 1;
+                }
             }
         }
     }
 
     fn view(&self) -> Element<Self::Message> {
         row![
-            button("Previous").on_press(Message::PreviousImage),
-            image::Image::new(image::Handle::from_path(&self.entries[self.i]))
+            button(" < ").on_press(Message::PreviousImage).padding([30,10]),
+            image::Viewer::new(image::Handle::from_path(&self.entries[self.page]))
             .width(Length::Fill).height(Length::Fill),
-            button("Next").on_press(Message::NextImage),
+            button(" > ").on_press(Message::NextImage).padding([30,10]),
         ]
-        .into()
-        //container::Container::new(image)
+        .align_items(Alignment::Center).into()
     }
 }
